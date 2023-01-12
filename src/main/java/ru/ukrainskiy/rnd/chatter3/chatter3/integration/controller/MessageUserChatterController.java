d7f544dce9bf1d11d76e3a0bc0a992fd552c8514
ukrainskiy40 <ukrainsky@kaluga.ru>
2023-01-12 12:58:07 +0300

    fix imports

--- src/main/java/ru/ukrainskiy/rnd/chatter3/chatter3/integration/controller/MessageUserChatterController.java
+++ src/main/java/ru/ukrainskiy/rnd/chatter3/chatter3/integration/controller/MessageUserChatterController.java
@@ -2,7 +2,6 @@ package ru.ukrainskiy.rnd.chatter3.chatter3.integration.controller;
 
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -13,9 +12,9 @@ import ru.ukrainskiy.rnd.chatter3.chatter3.usecase.adapter.MessageUserChatterAda
 @RestController
 @RequiredArgsConstructor
 public class MessageUserChatterController implements MessageUserChatterApi {
-    
+
     private final MessageUserChatterAdapter adapter;
-    
+
     public List<MessageUserChatterViewDto> findAllMyMessage() {
         return adapter.findAllMessageByUserId();
     }
